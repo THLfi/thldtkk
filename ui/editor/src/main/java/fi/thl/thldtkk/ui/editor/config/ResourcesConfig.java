1b1166c47dd05618dada268cc76ce2417a3f0717
Pekka <pekka.simola@thl.fi>
2017-04-25 15:15:08 +0300

    Enabled the angular2 application, the old application was loaded previously
    Issue: THLDTKK-52
    Change-Id: I52015606f416323232cac178db1d8505bc152402

--- ui/editor/src/main/java/fi/thl/thldtkk/ui/editor/config/ResourcesConfig.java
+++ ui/editor/src/main/java/fi/thl/thldtkk/ui/editor/config/ResourcesConfig.java
@@ -3,7 +3,6 @@ package fi.thl.thldtkk.ui.editor.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -12,7 +11,7 @@ import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
 
-@Configuration
+//@Configuration
 public class ResourcesConfig extends WebMvcConfigurerAdapter {
 
     private static final Logger LOG = LoggerFactory.getLogger(ResourcesConfig.class);
--- ui/editor/src/main/java/fi/thl/thldtkk/ui/editor/controller/RootController.java
+++ ui/editor/src/main/java/fi/thl/thldtkk/ui/editor/controller/RootController.java
@@ -3,11 +3,10 @@ package fi.thl.thldtkk.ui.editor.controller;
 import javax.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+//@Controller
 public class RootController {
 
     @Autowired

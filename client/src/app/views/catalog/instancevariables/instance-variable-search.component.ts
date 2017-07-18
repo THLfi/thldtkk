8e275243dcdb970233d8490fbf3054587b830edf
Simo Mäkinen <simo.v.makinen@thl.fi>
2017-07-18 17:09:15 +0300

    Instance variable search now updates query string in address and thus keeps it in user's history.
    Change-Id: I5b8e705ec261be66b1a66da38337e377e07b1c92
    Issue-ID: THLDTKK-224

--- client/src/app/views/catalog/instancevariables/instance-variable-search.component.ts
+++ client/src/app/views/catalog/instancevariables/instance-variable-search.component.ts
@@ -1,7 +1,8 @@
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router, UrlTree } from '@angular/router'
 import { Component, OnInit } from '@angular/core'
-import { Observable } from 'rxjs'
+import { Observable, Subscription } from 'rxjs'
 import { TranslateService } from '@ngx-translate/core'
+import { Location } from '@angular/common'
 
 import { Dataset } from '../../../model2/dataset'
 import { DatasetService } from '../../../services2/dataset.service'
@@ -17,10 +18,13 @@ export class InstanceVariableSearchComponent implements OnInit {
 
   language: string
   instanceVariables: InstanceVariable[]
+  instanceVariableSearchSubscription: Subscription
   searchText: string
 
   constructor(private instanceVariableService: InstanceVariableService,
               private route: ActivatedRoute,
+              private router: Router,
+              private location: Location,
               private translateService: TranslateService) {
     this.language = this.translateService.currentLang
   }
@@ -37,8 +41,21 @@ export class InstanceVariableSearchComponent implements OnInit {
   }
 
   searchInstanceVariables(searchText: string) {
-        this.instanceVariableService.searchInstanceVariable(searchText)
-          .subscribe(instanceVariables => this.instanceVariables = instanceVariables)
+    if(this.instanceVariableSearchSubscription) {
+      this.instanceVariableSearchSubscription.unsubscribe();
+    }
+
+    this.instanceVariableSearchSubscription = this.instanceVariableService.searchInstanceVariable(searchText)
+      .subscribe(instanceVariables => this.instanceVariables = instanceVariables)
+
+    this.updateQueryParam(searchText)
   }
 
+  private updateQueryParam(searchText:string):void {
+    let urlTree:UrlTree = this.router.parseUrl(this.router.url)
+    urlTree.queryParams['query'] = this.searchText
+
+    let updatedUrl = this.router.serializeUrl(urlTree);
+    this.location.replaceState(updatedUrl);
+  }
 }

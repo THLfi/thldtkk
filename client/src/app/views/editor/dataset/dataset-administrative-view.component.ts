5c58e5c008c0cbac37e248154b5390e23cd7b645
Pekka <pekka.simola@thl.fi>
2017-10-11 15:29:06 +0300

    Administrative information view - saving enabled but no fields are active yet
    Issue: THLDTKK-339
    Change-Id: I0ee3d6d3b9aedbfd2672981eeecf4ecf1acfffd7

---
+++ client/src/app/views/editor/dataset/dataset-administrative-view.component.ts
@@ -0,0 +1,52 @@
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from "@ngx-translate/core";
+import { Title } from '@angular/platform-browser'
+import { LangPipe } from '../../../utils/lang.pipe'
+
+import { Dataset } from "../../../model2/dataset";
+import { EditorDatasetService } from '../../../services-editor/editor-dataset.service'
+import { SidebarActiveSection } from './sidebar/sidebar-active-section'
+
+@Component({
+  templateUrl: './dataset-administrative-view.component.html',
+  styleUrls: [ './dataset-administrative-view.component.css' ]
+})
+export class DatasetAdministrativeViewComponent implements OnInit {
+
+  dataset: Dataset
+  language: string
+
+  sidebarActiveSection = SidebarActiveSection.ADMINISTRATIVE_INFORMATION
+
+  constructor(private datasetService: EditorDatasetService,
+              private route: ActivatedRoute,
+              private translateService: TranslateService,
+              private titleService: Title,
+              private langPipe: LangPipe) {
+    this.language = this.translateService.currentLang
+  }
+
+  ngOnInit() {
+    this.getDataSet();
+  }
+
+  getDataSet() {
+    const datasetId = this.route.snapshot.params['datasetId'];
+    this.datasetService.getDataset(datasetId)
+      .subscribe(dataset => {
+        this.dataset = dataset
+        this.updatePageTitle()
+      })
+  }
+
+  updatePageTitle():void {
+    if(this.dataset.prefLabel) {
+      let translatedLabel:string = this.langPipe.transform(this.dataset.prefLabel)
+      let bareTitle:string = this.titleService.getTitle();
+      this.titleService.setTitle(translatedLabel + " - " + bareTitle)
+    }
+  }
+
+
+}

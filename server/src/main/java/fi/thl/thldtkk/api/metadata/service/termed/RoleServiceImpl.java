b8318737c8275d4f0085b009c14741869af0b107
Jussi Kurki <jussi.kurki@thl.fi>
2019-01-23 15:08:20 +0200

    Fix bug where user roles was left empty
    Also replace plus icon with pencil in edit buttons.
    
    Issue: THLDTKK-569
    Change-Id: I1ac86398b2ad94557aea691d55dd897cdb2c21b8

--- server/src/main/java/fi/thl/thldtkk/api/metadata/service/termed/RoleServiceImpl.java
+++ server/src/main/java/fi/thl/thldtkk/api/metadata/service/termed/RoleServiceImpl.java
@@ -7,16 +7,16 @@ import static fi.thl.thldtkk.api.metadata.util.Tokenizer.tokenizeAndMap;
 import static java.util.stream.Collectors.toList;
 
 import fi.thl.thldtkk.api.metadata.domain.Role;
+import fi.thl.thldtkk.api.metadata.domain.query.Criteria;
 import fi.thl.thldtkk.api.metadata.domain.termed.Node;
 import fi.thl.thldtkk.api.metadata.domain.termed.NodeId;
 import fi.thl.thldtkk.api.metadata.security.annotation.UserCanCreateAdminCanUpdate;
 import fi.thl.thldtkk.api.metadata.service.Repository;
 import fi.thl.thldtkk.api.metadata.service.RoleService;
-import org.springframework.security.access.method.P;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.security.access.method.P;
 
 public class RoleServiceImpl implements RoleService {
 
@@ -35,12 +35,13 @@ public class RoleServiceImpl implements RoleService {
 
   @Override
   public List<Role> find(String query, int max) {
-    return nodes.query(
-        and(keyValue("type.id", "Role"),
-            keyWithAllValues("properties.prefLabel", tokenizeAndMap(query, t -> t + "*"))),
-        max)
-        .map(Role::new)
-        .collect(toList());
+    Criteria criteria = query.isEmpty()
+      ? keyValue("type.id", "Role")
+      : and(
+          keyValue("type.id", "Role"),
+          keyWithAllValues("properties.prefLabel", tokenizeAndMap(query, t -> t + "*")));
+
+    return nodes.query(criteria, max).map(Role::new).collect(toList());
   }
 
   @Override

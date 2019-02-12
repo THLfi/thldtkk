1058fe42e28ac1a1a1662506acec5fec21091fb2
Henri Hesse <henri.hesse@thl.fi>
2019-02-12 12:43:47 +0200

    Added study description and purpose of person registrer field to scientific privacy notice pdf
    Issue: THLDTKK-607
    Change-Id: I83469be00865affb8a12ed8960c71188105ea068

--- server/src/main/java/fi/thl/thldtkk/api/metadata/service/impl/EditorStudyPrivacyNoticeServiceImpl.java
+++ server/src/main/java/fi/thl/thldtkk/api/metadata/service/impl/EditorStudyPrivacyNoticeServiceImpl.java
@@ -167,6 +167,7 @@ public class EditorStudyPrivacyNoticeServiceImpl implements StudyPrivacyNoticeSe
       }
     }
 
+    context.setVariable("description", study.getDescription().get(lang));
     context.setVariable("studyName", study.getPrefLabel().get(lang));
     context.setVariable("purposeOfPersonRegister", study.getPurposeOfPersonRegistry().get(lang));
     context.setVariable("usageOfPersonalInformation", study.getUsageOfPersonalInformation().get(lang));

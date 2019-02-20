f516f12265d03237f1d388edca9476b387b48246
Max Salminen <max.salminen@valamis.com>
2019-02-20 09:36:47 +0200

    Initialize studyPerformers in javascript
    Fix issue where new study couldn't be created due to uninitialized
    studyPerformers variable.
    
    Issue: THLDTKK-610
    Change-Id: Ib706f8273ceaa3c66cc924856b975255774ce321

--- client/src/app/services-editor/editor-study.service.ts
+++ client/src/app/services-editor/editor-study.service.ts
@@ -81,6 +81,7 @@ export class EditorStudyService {
       'otherLegalBasisForHandlingSensitivePersonalData',
       'otherTypeOfSensitivePersonalData',
       'partiesAndSharingOfResponsibilityInCollaborativeStudy',
+      'studyPerformers',
       'profilingAndAutomationDescription',
       'groundsForConfidentiality'
     ])
